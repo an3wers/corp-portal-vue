128cffb319a94f8214707a204d23b9948a83f223
an3wers <an3wers@gmail.com>
2022-04-05 13:08:49 +0500

    init proj

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap';
+import '@/scss/index.scss'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import '@/scss/index.scss'
+
+createApp(App).use(store).use(router).mount('#app')
